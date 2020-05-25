a3f858b0a17ee7ac7cee76b522a8e3143a271f67
Lynn-Ngx <C15426672@mydit.ie>
2020-05-25 02:10:28 +0100

    User Interface for logging system

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Fragment, useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+
+const App = () => {
+  useEffect(() => {
+    // Auto initialise Materialize JS
+    M.AutoInit();
+  })
+  return (
+    <Fragment>
+
+        <SearchBar/>
+        <div className="container">
+            <AddBtn/>
+            <AddLogModal/>
+            <Logs/>
+        </div>
+    </Fragment>
+  );
+}
+
+export default App;
